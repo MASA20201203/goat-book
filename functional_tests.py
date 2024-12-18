18b4e358174dafdef372b482906fd265f68923b2
MASA20201203 <koedame032@gmail.com>
2024-12-18 17:58:21 +0900

    update: ch04 機能テストでTodoの項目を入力できるかチェックできるよう更新

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,18 +19,29 @@ class NewVisitorTest(unittest.TestCase):
 
         # 彼女はページのタイトルとヘッダーがToDoリストについて言及していることに気づく
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
 
         # すぐにToDoを入力するよう促される
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # 彼女はテキストボックスに「孔雀の羽を買う」と入力する
         # (エディスの趣味はフライフィッシングのルアーを結ぶこと)
+        inputbox.send_keys("孔雀の羽を買う")
 
         # 彼女がエンターキーを押すと、ページが更新され、次のように表示される。
         # 「1: クジャクの羽を買う」 がToDoリストの項目としてリストアップされる
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: 孔雀の羽を買う" for row in rows))
 
         # 別の項目を追加するよう促すテキストボックスが残っている。
         # 彼女は 「孔雀の羽を使ってハエを作る 」と入力した。
+        self.fail("Finish the test!")
 
         # ページが再び更新され、リストの両方の項目が表示される。
 
