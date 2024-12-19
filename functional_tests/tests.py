3e7da4ad1bea09068d6ee9f79cdca9f425112352
MASA20201203 <koedame032@gmail.com>
2024-12-19 13:23:58 +0900

    update: function_test をアプリにして、LiveServerTestCaseを使用

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -5,7 +6,7 @@ import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_todo_list(self):
         # エディスは、クールな新しいオンラインToDoアプリについて聞いた。
         # 彼女はそのホームページをチェックしに行く
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # 彼女はページのタイトルとヘッダーがToDoリストについて言及していることに気づく
         self.assertIn("To-Do", self.browser.title)
@@ -53,7 +54,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table("2: 孔雀の羽を使ってハエを作る")
 
         # 満足した彼女は眠りにつく
-
-
-if __name__ == "__main__":
-    unittest.main()
