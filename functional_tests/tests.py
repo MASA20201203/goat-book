088bc403e67c2e2a6000a1796368faf1e0a38253
MASA20201203 <koedame032@gmail.com>
2024-12-19 14:42:29 +0900

    update: ch07 新しいユーザーストーリー（FT）を追加

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -62,3 +62,44 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table("2: 孔雀の羽を使ってハエを作る")
 
         # 満足した彼女は眠りにつく
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # エディスは新しいToDoリストを作成する
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("孔雀の羽を買う")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: 孔雀の羽を買う")
+
+        # 彼女は自分のリストに固有のURLが生成されたことを確認する
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+
+        # 新しいユーザ、フランシスがサイトにアクセスする
+
+        ## 真新しいユーザーセッションをシミュレートする方法として、ブラウザのクッキーをすべて削除する。
+        self.browser.delete_all_cookies()
+
+        # フランシスがアクセスし、エディスのリストの情報が見えないことを確認する
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("孔雀の羽を買う", page_text)
+        self.assertNotIn("孔雀の羽を使ってハエを作る", page_text)
+
+        # フランシスは新しい項目を入力することで、新しいリストを開始する。彼はエディスより面白くない...。
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("ミルクを買う")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: ミルクを買う")
+
+        # フランシスは彼女のリストに固有のURLが生成されたことを確認する
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # エディスのリストにはフランシスのリストの情報が表示されないことを確認する
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("孔雀の羽を買う", page_text)
+        self.assertIn("ミルクを買う", page_text)
+
+        # 二人は満足して眠りにつく
