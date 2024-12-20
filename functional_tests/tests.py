cabe1b4c1cd0af6b4bd907e3ba1f7be64f4c3c50
MASA20201203 <koedame032@gmail.com>
2024-12-20 16:02:24 +0900

    update: ch09 FTランナーをハックして、dokcerをテストできるようにする

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+import os
 import time
 
 MAX_WAIT = 5
@@ -11,6 +12,9 @@ MAX_WAIT = 5
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        test_server = os.environ.get("TEST_SERVER")
+        if test_server:
+            self.live_server_url = "http://" + test_server
 
     def tearDown(self):
         self.browser.quit()
