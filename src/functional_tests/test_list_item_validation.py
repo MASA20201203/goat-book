8238530229dc6b20fc3065cefb3cd341618211ac
MASA20201203 <koedame032@gmail.com>
2024-12-24 09:59:45 +0900

    update: ch15 すべてのビューでフォームを使用し、作業状態に戻る

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -13,32 +13,35 @@ class ItemValidationTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # ホームページがリフレッシュされ、エラーメッセージが表示された。
+        # ブラウザはリクエストをインターセプトし、リストページをロードしない。
         self.wait_for(
-            lambda: self.assertEqual(
-                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
-                "You can't have an empty list item",
-            )
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_text:invalid")
+        )
+
+        # 彼女は新しいアイテムのためにテキストを入力し始め、エラーは消えます。
+        self.get_item_input_box().send_keys("ミルクを買う")
+        self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_text:valid")
         )
 
         # 項目にテキストを追加して再試行すると、うまくいった
-        self.get_item_input_box().send_keys("ミルクを買う")
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: ミルクを買う")
 
         # ひょんなことから、彼女は2つ目の空白のリスト項目を投稿することにした。
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # 同じような警告がリストページに表示される
+        # 再び、ブラウザは応じない
+        self.wait_for_row_in_list_table("1: ミルクを買う")
         self.wait_for(
-            lambda: self.assertEqual(
-                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
-                "You can't have an empty list item",
-            )
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_text:invalid")
         )
 
-        # テキストを入力することで修正できる
+        # そして、彼女はテキストを入力することで満足させることができる。
         self.get_item_input_box().send_keys("紅茶を作る")
+        self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_text:valid")
+        )
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: ミルクを買う")
         self.wait_for_row_in_list_table("2: 紅茶を作る")
